9e1ff53cd6a90aae6c9ce91a93e26bc1e92aa4f9
Jack <71041042+Haxor1611@users.noreply.github.com>
2021-04-08 18:12:58 +0100

    Source

---
+++ Source/Builder/Builder/Builder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Builder
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("What do you want to write to the other exe.");
+
+			string settings = Console.ReadLine();
+			using (FileStream fileStream = new FileStream("Stub.exe", FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+			{
+				using (BinaryWriter binary = new BinaryWriter(fileStream))
+				{
+					fileStream.Position = fileStream.Length + 1;
+					binary.Write("/*/" + settings);
+				}
+			}
+			Console.WriteLine("Written");
+		}
+	}
+}
---
+++ Source/Stub/Stub/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Stub
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using(StreamReader streamReader = new StreamReader(System.Reflection.Assembly.GetEntryAssembly().Location))
+			{
+				using(BinaryReader binary = new BinaryReader(streamReader.BaseStream))
+				{
+					byte[] stubBytes = binary.ReadBytes(Convert.ToInt32(streamReader.BaseStream.Length));
+					//***
+					string stubSettings = Encoding.ASCII.GetString(stubBytes).Substring(Encoding.ASCII.GetString(stubBytes).IndexOf("/*/")).Replace("/*/", "");
+					Console.Clear();
+					Console.WriteLine(stubSettings);
+					Console.ReadLine();
+					Console.Clear();
+
+				}
+			}
+		}
+	}
+}
